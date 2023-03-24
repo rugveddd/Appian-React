6a1acddad778cea261b0a7742d422d8f0d081d65
rugveddd <116478342+rugveddd@users.noreply.github.com>
2023-03-24 13:20:43 +0530

    Update EmployeeDetails.js

--- src/EmployeeDetails.js
+++ src/EmployeeDetails.js
@@ -9,11 +9,15 @@ const EmployeeDetails = (props) => {
         const response = await fetch(
           "https://paix.appian.community/suite/webapi/appianEndpoint",
           {
+            method: 'GET',
             headers: {
               Authorization:
                 "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlZGI5MjJjYy02ODU4LTQxMDMtODYwZC0yN2YxMGY3MmFmNWUifQ.N_shBX0Pa5VIeJGIU4Ybz6ykFCk4Crlw7nfCEzxKAr8",
               "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": 'no-cors',
+              'Access-Control-Request-Method': 'GET',
+              "Access-Control-Allow-Origin": 'https://papaya-strudel-5d6793.netlify.app',
+              'Access-Control-Request-Headers': 'Authorization, Content-Type'
+              
               
             },
           }
